d90f63d6cceee254d32b764290a61d30c7d9192d
Pavel Dvoinos <pavel.dvoinos@transferwise.com>
2020-11-13 23:26:13 +0200

    change plain run to functions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,22 @@
 #!/bin/sh -l
 
-echo "Hello ${WHO_TO_GREET}!"
+greet() {
+  echo "Hello ${WHO_TO_GREET}!"
+}
 
+out_time() {
+  time=$(date)
+  echo "Time is"
+  echo "${time}"
+  echo "::set-output name=time::$time"
+}
 
-time=$(date)
-echo "Time is"
-echo "${time}"
-echo "::set-output name=time::$time"
+out_other() {
+  echo "Listing is "
+  ls -lah $DIR
+  LISTING=$(ls $DIR)
+  echo "::set-output name=listing::$LISTING"
 
-echo "Listing is "
-ls -lah $DIR
-LISTING=$(ls $DIR)
-echo "::set-output name=listing::$LISTING"
-
-echo "Set random"
-echo "::set-output name=random-id::$RANDOM"
+  echo "Set random"
+  echo "::set-output name=random-id::$RANDOM"
+}
